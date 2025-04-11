81907e7f7cd30abdc6a81785b01aaa0e43a031d0
kolane <11@qq.com>
2025-04-12 01:36:49 +0800

    feat(boost_demo): 新建FileTransferSession类

--- boost_demo/include/HttpSession.h
+++ boost_demo/include/HttpSession.h
@@ -1,3 +1,6 @@
+#ifndef BOOST_DEMO_HTTPSESSION_H
+#define BOOST_DEMO_HTTPSESSION_H
+
 #include <boost/asio.hpp>
 #include <filesystem>
 #include <fstream>
@@ -11,30 +14,25 @@ using tcp = asio::ip::tcp;
 
 class HttpSession : public std::enable_shared_from_this<HttpSession> {
 public:
-  explicit HttpSession(tcp::socket socket);
-  ~HttpSession();
-  void Start();
+    explicit HttpSession(tcp::socket socket);
+    ~HttpSession();
+    void Start();
 
 private:
-  void ReadRequest();
-  void HandleDownload(const std::string &path);
-  void DownloadRealFile(const std::string &filePath);
-  void DownloadVirtualFile(const size_t fileSize);
-  void SendFileChunk(std::ifstream file, std::streamsize fileSize);
-  void SendVirtualData(size_t sentBytes);
-  void ReadBody();
-  void SendResponse(const std::string &response);
-
-  // 服务端优雅关闭示例
-  void GracefulShutdown();
+    void ReadRequest();
+    void HandleDownload(const std::string &path);
+    void ReadBody();
+    void SendResponse(const std::string &response);
+    void GracefulShutdown();
 
 private:
-  tcp::socket socket_;
-  asio::streambuf request_buffer_;
-  std::ofstream output_file_;
-  std::filesystem::path temp_file_path_;
-  size_t content_length_ = 0;
-  size_t received_bytes_ = 0;
-  size_t file_size_ = 0;
-  std::string dummy_buffer_;
+    tcp::socket socket_;
+    asio::streambuf request_buffer_;
+    std::ofstream output_file_;
+    std::filesystem::path temp_file_path_;
+    size_t content_length_ = 0;
+    size_t received_bytes_ = 0;
+    std::string dummy_buffer_;
 };
+
+#endif //BOOST_DEMO_HTTPSESSION_H
